f6276302fef558e338b521cf64d46648eba411d3
nafiurrahmansabbir <nafiurrahmansabbir@gmail.com>
2024-05-21 17:18:28 +0600

    Done

--- lib/api_photo_and_details.dart
+++ lib/api_photo_and_details.dart
@@ -1,16 +1,16 @@
 class Photo {
-  final int albumId;
-  final int id;
-  final String title;
-  final String url;
-  final String thumbnailUrl;
+      final int albumId;
+      final int id;
+      final String title;
+      final String url;
+      final String thumbnailUrl;
 
   Photo({
-    required this.albumId,
-    required this.id,
-    required this.title,
-    required this.url,
-    required this.thumbnailUrl,
+      required this.albumId,
+      required this.id,
+      required this.title,
+      required this.url,
+      required this.thumbnailUrl,
   });
 
   factory Photo.fromJson(Map<String, dynamic> json) {
--- lib/imgDetailsPage.dart
+++ lib/imgDetailsPage.dart
@@ -10,6 +10,7 @@ class details extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -20,10 +21,38 @@ class details extends StatelessWidget {
         backgroundColor: Colors.blue,
         title: Text('Photo Details',style: TextStyle(fontSize: 23,color: Colors.white,fontWeight: FontWeight.w500)),
       ),
-      body: Center(
-        child: Image.network(url),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          // mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 80,left: 20,right: 20),
+              child: SizedBox(
+                  height: .9*screenSize.width, width: .9*screenSize.width, child: Image.network(url,fit: BoxFit.cover,)
+              ),
+            ),
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 20,left: 20,right: 20),
+                  child: Text("Title: $title",style: TextStyle(fontSize: 18),softWrap:true,maxLines: 2,),
+                )
+              ],
+        
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("ID : $Id",style: TextStyle(fontSize: 18),),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
 }
+
+
 
